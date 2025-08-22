253eb3125a0d4128db37286d024ba234726a5160
shiamrahman2 <mohammodshiambhuiyan@gmail.com>
2025-08-23 01:18:08 +0600

    createElement

---
+++ js/childJs.js
@@ -0,0 +1,4 @@
+const newchild=document.createElement('li');
+newchild.innerText='My Favourite fruit is Mango';
+const parentNode=document.getElementById('fruit-collection');
+parentNode.appendChild(newchild);
